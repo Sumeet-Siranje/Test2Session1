b0c32d333cd7d5b8128b6701d769aafdc06cc0ab
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-02-28 06:49:07 +0000

    p2original.c_done

--- p2original.c
+++ p2original.c
@@ -15,6 +15,7 @@ int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
   else
     return 0;   
 }
+
 void output(float x1, float y1, float x2, float y2,float x3, float y3,int istriangle){
   if(is_triangle==1){
     printf("the given side form atriangle\n");
